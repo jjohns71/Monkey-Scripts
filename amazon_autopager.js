81d92b50d54f01bb94c3654d2b413bbb3c4fdf13
jjohns71 <joshua.s.johnson@gmail.com>
2017-07-12 00:29:39 -0400

    Update amazon_autopager.js

--- amazon_autopager.js
+++ amazon_autopager.js
@@ -1,22 +1,22 @@
 // ==UserScript==
-// @name			Amazon Autopager (Infinite Scroll)
-// @version			1.04.1
-// @icon			https://www.amazon.com/favicon.ico
-// @description		Loads next page of search results automatically on Amazon
-// @namespace		https://github.com/jjohns71/Monkey-Scripts
-// @author			Original: https://greasyfork.org/en/scripts/23471-amazon-autopager
-// @contributor		jjohns71
-// @include			https://www.amazon.com/s/*
-// @include			https://smile.amazon.com/s/*
-// @include			https://www.amazon.co.uk/s/*
-// @include			https://www.amazon.co.jp/s/*
-// @grant			none
-// @homepage	    https://github.com/jjohns71/Monkey-Scripts/blob/master/amazon_autopager.js
-// @downloadURL 	https://raw.githubusercontent.com/jjohns71/Monkey-Scripts/master/amazon_autopager.js
+// @name            Amazon Autopager (Infinite Scroll)
+// @version         1.04.2
+// @icon            https://www.amazon.com/favicon.ico
+// @description     Loads next page of search results automatically on Amazon
+// @namespace       https://github.com/jjohns71/Monkey-Scripts
+// @author          Original: https://greasyfork.org/en/scripts/23471-amazon-autopager
+// @contributor     jjohns71
+// @include         https://www.amazon.com/s/*
+// @include         https://smile.amazon.com/s/*
+// @include         https://www.amazon.co.uk/s/*
+// @include         https://www.amazon.co.jp/s/*
+// @grant           none
+// @homepage        https://github.com/jjohns71/Monkey-Scripts/blob/master/amazon_autopager.js
+// @downloadURL     https://raw.githubusercontent.com/jjohns71/Monkey-Scripts/master/amazon_autopager.js
 // ==/UserScript==
 
 (function () {
-'use strict';
+	'use strict';
     function fscroll()
     {
         var	next=D.getElementById('pagnNextLink'),
